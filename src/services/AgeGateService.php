2d75e07b8419bed57e8eb29c9e9b07ebbfae264d
Tiago Dias <funkysoulzh@gmail.com>
2023-03-22 20:14:16 +0100

    Fix: Migration Errors

--- src/services/AgeGateService.php
+++ src/services/AgeGateService.php
@@ -8,8 +8,7 @@ use craft\helpers\UrlHelper;
 use craft\web\View;
 use thekitchenagency\craftagegate\records\SettingsRecord;
 
-class AgeGateService extends Component
-{
+class AgeGateService extends Component {
 	public static $settings = null;
 
 	/**
@@ -17,8 +16,9 @@ class AgeGateService extends Component
 	 * @return SettingsRecord
 	 */
 	public function getCurrentSiteAgeGateSettings(): SettingsRecord {
-		$params = ['agegateSiteId' => Craft::$app->sites->currentSite->id];
-		return SettingsRecord::findOne($params);
+		$params = [ 'agegateSiteId' => Craft::$app->sites->currentSite->id ];
+
+		return SettingsRecord::findOne( $params );
 	}
 
 	/**
@@ -26,44 +26,46 @@ class AgeGateService extends Component
 	 * @return void
 	 */
 	public function initRedirectionAgegate(): void {
-		self::$settings = $this->getCurrentSiteAgeGateSettings();
+		if ( Craft::$app->request->getIsSiteRequest() ) {
+			self::$settings = $this->getCurrentSiteAgeGateSettings();
 
-		$entry = [];
-		if(self::$settings->pagePrivacyPolicy) {
-			$entry[] = Craft::$app->getEntries()->getEntryById(json_decode(self::$settings->pagePrivacyPolicy)[0]);
-		}
+			$entry = [];
+			if ( self::$settings->pagePrivacyPolicy ) {
+				$entry[] = Craft::$app->getEntries()->getEntryById( json_decode( self::$settings->pagePrivacyPolicy )[0] );
+			}
 
-		if(self::$settings->pageCookiePolicy) {
-			$entry[] = Craft::$app->getEntries()->getEntryById(json_decode(self::$settings->pageCookiePolicy)[0]);
-		}
+			if ( self::$settings->pageCookiePolicy ) {
+				$entry[] = Craft::$app->getEntries()->getEntryById( json_decode( self::$settings->pageCookiePolicy )[0] );
+			}
 
-		$matchingSite = false;
-		if($entry) {
-			foreach ( $entry as $singleEntry ) {
-				if ( Craft::$app->getRequest()->getSegment(1) === $singleEntry->slug ) {
-					$matchingSite = true;
+			$matchingSite = false;
+			if ( $entry ) {
+				foreach ( $entry as $singleEntry ) {
+					if ( Craft::$app->getRequest()->getSegment( 1 ) === $singleEntry->slug ) {
+						$matchingSite = true;
+					}
 				}
 			}
-		}
 
-		if(self::$settings->isAgeGateEnabled && ! $matchingSite  && self::$settings->displayType === 'redirect') {
-			if ( Craft::$app->request->getIsSiteRequest() ) {
-				if ( self::$settings->isAgeGateEnabled && !$matchingSite && self::$settings->displayType === 'redirect' && Craft::$app->getRequest()->getSegment( 1 ) != 'agegate' ) {
-					$originalUrl = Craft::$app->getRequest()->getFullUri();
-					Craft::$app->getSession()->set( 'originalSrcUrl', $originalUrl );
+			if ( self::$settings->isAgeGateEnabled && ! $matchingSite && self::$settings->displayType === 'redirect' ) {
+				if ( Craft::$app->request->getIsSiteRequest() ) {
+					if ( self::$settings->isAgeGateEnabled && ! $matchingSite && self::$settings->displayType === 'redirect' && Craft::$app->getRequest()->getSegment( 1 ) != 'agegate' ) {
+						$originalUrl = Craft::$app->getRequest()->getFullUri();
+						Craft::$app->getSession()->set( 'originalSrcUrl', $originalUrl );
 
-					if ( ! isset( $_COOKIE[ self::$settings->cookieName ] ) || empty( $_COOKIE[ self::$settings->cookieName ] ) ) {
-						Craft::$app->getResponse()->redirect( UrlHelper::siteUrl( 'agegate' ) )->send();
-					}
+						if ( ! isset( $_COOKIE[ self::$settings->cookieName ] ) || empty( $_COOKIE[ self::$settings->cookieName ] ) ) {
+							Craft::$app->getResponse()->redirect( UrlHelper::siteUrl( 'agegate' ) )->send();
+						}
 
-				} else if ( self::$settings->isAgeGateEnabled && ! $matchingSite && self::$settings->displayType === 'redirect' && Craft::$app->getRequest()->getSegment( 1 ) == 'agegate' ) {
-					Craft::$app->getView()->registerJsVar( 'originalSrcUrl', Craft::$app->getSession()->get( 'originalSrcUrl' ) );
+					} else if ( self::$settings->isAgeGateEnabled && ! $matchingSite && self::$settings->displayType === 'redirect' && Craft::$app->getRequest()->getSegment( 1 ) == 'agegate' ) {
+						Craft::$app->getView()->registerJsVar( 'originalSrcUrl', Craft::$app->getSession()->get( 'originalSrcUrl' ) );
 
-					if ( isset( $_COOKIE[ self::$settings->cookieName ] ) || !empty( $_COOKIE[ self::$settings->cookieName ] ) ) {
+						if ( isset( $_COOKIE[ self::$settings->cookieName ] ) || ! empty( $_COOKIE[ self::$settings->cookieName ] ) ) {
+							Craft::$app->getResponse()->redirect( Craft::$app->getSession()->get( 'originalSrcUrl' ) )->send();
+						}
+					} else {
 						Craft::$app->getResponse()->redirect( Craft::$app->getSession()->get( 'originalSrcUrl' ) )->send();
 					}
-				} else {
-					Craft::$app->getResponse()->redirect( Craft::$app->getSession()->get( 'originalSrcUrl' ) )->send();
 				}
 			}
 		}
@@ -75,12 +77,12 @@ class AgeGateService extends Component
 	 */
 	public function renderAgeGate(): bool {
 		// DISPLAY ONLY ON FRONTEND
-		if( !Craft::$app->request->getIsSiteRequest() ) {
+		if ( ! Craft::$app->request->getIsSiteRequest() ) {
 			return false;
 		}
 
-		if ( !isset($_COOKIE[self::$settings->cookieName]) && empty($_COOKIE[self::$settings->cookieName]) ) {
-			$html = Craft::$app->view->renderTemplate('_agegate/index.twig', ['settings' => self::$settings], View::TEMPLATE_MODE_SITE);
+		if ( ! isset( $_COOKIE[ self::$settings->cookieName ] ) && empty( $_COOKIE[ self::$settings->cookieName ] ) ) {
+			$html = Craft::$app->view->renderTemplate( '_agegate/index.twig', [ 'settings' => self::$settings ], View::TEMPLATE_MODE_SITE );
 			echo $html;
 		}
 
